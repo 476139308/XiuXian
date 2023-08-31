0eb4773257ad611df9d687254f53bff7665ed79c
yujin <476139308@qq.com>
2023-08-31 12:46:57 +0800

    feat: 1.初始化项目

---
+++ src/main/java/com/yj/xiuxian/XiuXianApplication.java
@@ -0,0 +1,13 @@
+package com.yj.xiuxian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiuXianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiuXianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yj/xiuxian/XiuXianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.xiuxian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiuXianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
